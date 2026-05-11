d96153eed5ffd7f8d29b9d8ace2ffb7216334201
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 21:05:46 +0900

    Create renderHands.js

---
+++ src/ui/renderHands.js
@@ -0,0 +1,43 @@
+// src/ui/renderHands.js
+
+import {
+  PIECES
+} from '../core/pieceData'
+
+export function renderHands(
+  app,
+  game
+) {
+
+  const root =
+    document.createElement('div')
+
+  root.className =
+    'hands'
+
+  const player =
+    document.createElement('div')
+
+  player.className =
+    'hand'
+
+  game.playerHand.forEach(
+    piece => {
+
+      const div =
+        document.createElement('div')
+
+      div.className =
+        'hand-piece'
+
+      div.textContent =
+        PIECES[piece].emoji
+
+      player.appendChild(div)
+    }
+  )
+
+  root.appendChild(player)
+
+  app.appendChild(root)
+}
