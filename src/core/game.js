6c294dfaaf2ff1a36b5a873acea712ef43e18557
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 21:28:43 +0900

    Update game.js

--- src/core/game.js
+++ src/core/game.js
@@ -21,21 +21,37 @@ export class Game {
 
   constructor() {
 
+    // 盤面
     this.board =
       createInitialBoard()
 
+    // 選択中盤面駒
     this.selected =
       null
 
+    // 選択中持ち駒
+    this.selectedHandPiece =
+      null
+
+    // 手番
     this.turn =
       'player'
 
+    // 持ち駒
     this.playerHand = []
 
     this.enemyHand = []
   }
 
+  // =========================
+  // 盤面駒選択
+  // =========================
+
   select(x, y) {
+
+    // 持ち駒選択解除
+    this.selectedHandPiece =
+      null
 
     const piece =
       this.board[y][x]
@@ -48,6 +64,7 @@ export class Game {
       return []
     }
 
+    // プレイヤーターンで敵駒触れない
     if (
       this.turn === 'player' &&
       isEnemyPiece(piece)
@@ -55,6 +72,7 @@ export class Game {
       return []
     }
 
+    // CPUターンで自駒触れない
     if (
       this.turn === 'enemy' &&
       isPlayerPiece(piece)
@@ -74,6 +92,10 @@ export class Game {
     )
   }
 
+  // =========================
+  // 駒移動
+  // =========================
+
   move(toX, toY) {
 
     if (!this.selected) {
@@ -89,7 +111,7 @@ export class Game {
     const target =
       this.board[toY][toX]
 
-    // 持ち駒化
+    // 駒取得
     if (target) {
 
       const captured =
@@ -98,29 +120,96 @@ export class Game {
       if (
         this.turn === 'player'
       ) {
+
         this.playerHand.push(
           captured
         )
       }
       else {
+
         this.enemyHand.push(
           captured
         )
       }
     }
 
+    // 移動
     this.board[toY][toX] =
       movingPiece
 
     this.board[from.y][from.x] =
       null
 
+    // 選択解除
     this.selected =
       null
 
+    // ターン変更
     this.turn =
       this.turn === 'player'
         ? 'enemy'
         : 'player'
+  }
+
+  // =========================
+  // 持ち駒選択
+  // =========================
+
+  selectHandPiece(piece) {
+
+    // 盤面選択解除
+    this.selected =
+      null
+
+    this.selectedHandPiece =
+      piece
+  }
+
+  // =========================
+  // 持ち駒配置
+  // =========================
+
+  drop(toX, toY) {
+
+    // 選択なし
+    if (
+      !this.selectedHandPiece
+    ) {
+      return false
+    }
+
+    // 空きマスのみ
+    if (
+      this.board[toY][toX]
+    ) {
+      return false
+    }
+
+    // 配置
+    this.board[toY][toX] =
+      this.selectedHandPiece
+
+    // 持ち駒から削除
+    const index =
+      this.playerHand.indexOf(
+        this.selectedHandPiece
+      )
+
+    if (index >= 0) {
+
+      this.playerHand.splice(
+        index,
+        1
+      )
+    }
+
+    // 選択解除
+    this.selectedHandPiece =
+      null
+
+    // ターン変更
+    this.turn = 'enemy'
+
+    return true
   }
 }
