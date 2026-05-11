07c4c154440c33e7adec0fff358eb073267fcf0e
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 20:31:30 +0900

    Update pieceData.js

--- src/core/pieceData.js
+++ src/core/pieceData.js
@@ -1,9 +1,44 @@
+// src/core/pieceData.js
+
 export const PIECES = {
+
   chick: {
-    emoji: '🐥'
+    emoji: '🐥',
+    name: 'ひよこ'
   },
 
-  enemy: {
-    emoji: '🐤'
+  enemyChick: {
+    emoji: '🐤',
+    name: 'ひよこ'
+  },
+
+  lion: {
+    emoji: '🦁',
+    name: 'ライオン'
+  },
+
+  enemyLion: {
+    emoji: '🦁',
+    name: 'ライオン'
+  },
+
+  giraffe: {
+    emoji: '🦒',
+    name: 'きりん'
+  },
+
+  enemyGiraffe: {
+    emoji: '🦒',
+    name: 'きりん'
+  },
+
+  elephant: {
+    emoji: '🐘',
+    name: 'ぞう'
+  },
+
+  enemyElephant: {
+    emoji: '🐘',
+    name: 'ぞう'
   }
 }
