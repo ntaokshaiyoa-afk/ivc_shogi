eec685f7a0890f3f5b44f8056398b1ee5a0619bd
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 20:33:08 +0900

    Update judge.js

--- src/core/judge.js
+++ src/core/judge.js
@@ -2,39 +2,30 @@
 
 export function checkWinner(board) {
 
-  let chickExists = false
-  let enemyExists = false
+  let playerLion = false
+  let enemyLion = false
 
-  for (let y = 0; y < board.length; y++) {
-    for (let x = 0; x < board[y].length; x++) {
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 3; x++) {
 
-      const piece = board[y][x]
+      const piece =
+        board[y][x]
 
-      if (piece === 'chick') {
-        chickExists = true
-
-        // 上端到達
-        if (y === 0) {
-          return 'player'
-        }
+      if (piece === 'lion') {
+        playerLion = true
       }
 
-      if (piece === 'enemy') {
-        enemyExists = true
-
-        // 下端到達
-        if (y === 3) {
-          return 'enemy'
-        }
+      if (piece === 'enemyLion') {
+        enemyLion = true
       }
     }
   }
 
-  if (!enemyExists) {
+  if (!enemyLion) {
     return 'player'
   }
 
-  if (!chickExists) {
+  if (!playerLion) {
     return 'enemy'
   }
 
