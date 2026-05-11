ef180118cb94d7ffee6ef5ed5925e0eb4d72c6c6
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 20:32:46 +0900

    Update rules.js

--- src/core/rules.js
+++ src/core/rules.js
@@ -1,5 +1,38 @@
-export function getLegalMoves(board, x, y) {
-  const piece = board[y][x]
+// src/core/rules.js
+
+function inside(x, y) {
+
+  return (
+    x >= 0 &&
+    x < 3 &&
+    y >= 0 &&
+    y < 4
+  )
+}
+
+function isEnemy(piece, target) {
+
+  if (!target) {
+    return true
+  }
+
+  const player =
+    !piece.startsWith('enemy')
+
+  const targetPlayer =
+    !target.startsWith('enemy')
+
+  return player !== targetPlayer
+}
+
+export function getLegalMoves(
+  board,
+  x,
+  y
+) {
+
+  const piece =
+    board[y][x]
 
   if (!piece) {
     return []
@@ -7,26 +40,78 @@ export function getLegalMoves(board, x, y) {
 
   const moves = []
 
-  if (piece === 'chick') {
-    const ny = y - 1
+  function add(dx, dy) {
 
-    if (ny >= 0) {
+    const nx = x + dx
+    const ny = y + dy
+
+    if (!inside(nx, ny)) {
+      return
+    }
+
+    const target =
+      board[ny][nx]
+
+    if (
+      isEnemy(piece, target)
+    ) {
       moves.push({
-        x,
+        x: nx,
         y: ny
       })
     }
   }
 
-  if (piece === 'enemy') {
-    const ny = y + 1
+  // ひよこ
+  if (piece === 'chick') {
+    add(0, -1)
+  }
 
-    if (ny < 4) {
-      moves.push({
-        x,
-        y: ny
-      })
-    }
+  if (piece === 'enemyChick') {
+    add(0, 1)
+  }
+
+  // ライオン
+  if (
+    piece === 'lion' ||
+    piece === 'enemyLion'
+  ) {
+
+    add(-1, -1)
+    add(0, -1)
+    add(1, -1)
+
+    add(-1, 0)
+    add(1, 0)
+
+    add(-1, 1)
+    add(0, 1)
+    add(1, 1)
+  }
+
+  // きりん
+  if (
+    piece === 'giraffe' ||
+    piece === 'enemyGiraffe'
+  ) {
+
+    add(0, -1)
+    add(-1, 0)
+    add(1, 0)
+    add(0, 1)
+  }
+
+  // ぞう
+  if (
+    piece === 'elephant' ||
+    piece === 'enemyElephant'
+  ) {
+
+    add(-1, -1)
+    add(1, -1)
+
+    add(-1, 1)
+    add(1, 1)
   }
 
   return moves
