01e9529ea6f1364c89c539501e37d3cacc0d616f
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 20:35:01 +0900

    Create effects.js

---
+++ src/ui/effects.js
@@ -0,0 +1,29 @@
+// src/ui/effects.js
+
+export function bouncePiece(
+  element
+) {
+
+  element.animate(
+    [
+      {
+        transform:
+          'scale(1)'
+      },
+
+      {
+        transform:
+          'scale(1.18)'
+      },
+
+      {
+        transform:
+          'scale(1)'
+      }
+    ],
+
+    {
+      duration: 220
+    }
+  )
+}
