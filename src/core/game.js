2a1fe2e00fac7489a7306f9a5dfee34e19b6a67b
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 20:17:55 +0900

    Update game.js

--- src/core/game.js
+++ src/core/game.js
@@ -1,27 +1,55 @@
+// src/core/game.js
+
 import { createInitialBoard } from './board'
 import { getLegalMoves } from './rules'
 
 export class Game {
+
   constructor() {
-    this.board = createInitialBoard()
+
+    this.board =
+      createInitialBoard()
 
     this.selected = null
+
+    this.turn = 'player'
   }
 
   select(x, y) {
-    const piece = this.board[y][x]
+
+    const piece =
+      this.board[y][x]
 
     if (!piece) {
       this.selected = null
       return []
     }
 
+    if (
+      this.turn === 'player' &&
+      piece !== 'chick'
+    ) {
+      return []
+    }
+
+    if (
+      this.turn === 'enemy' &&
+      piece !== 'enemy'
+    ) {
+      return []
+    }
+
     this.selected = { x, y }
 
-    return getLegalMoves(this.board, x, y)
+    return getLegalMoves(
+      this.board,
+      x,
+      y
+    )
   }
 
   move(toX, toY) {
+
     if (!this.selected) {
       return
     }
@@ -39,5 +67,10 @@ export class Game {
       null
 
     this.selected = null
+
+    this.turn =
+      this.turn === 'player'
+        ? 'enemy'
+        : 'player'
   }
 }
