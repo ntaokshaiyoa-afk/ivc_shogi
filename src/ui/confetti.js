9901dc7f0f06de4c0aea1ddbd40afdce9bbbc19e
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 21:20:11 +0900

    Create confetti.js

---
+++ src/ui/confetti.js
@@ -0,0 +1,28 @@
+// src/ui/confetti.js
+
+export function launchConfetti() {
+
+  for (let i = 0; i < 80; i++) {
+
+    const div =
+      document.createElement('div')
+
+    div.className =
+      'confetti'
+
+    div.style.left =
+      `${Math.random() * 100}vw`
+
+    div.style.animationDelay =
+      `${Math.random() * 0.5}s`
+
+    div.style.transform =
+      `rotate(${Math.random() * 360}deg)`
+
+    document.body.appendChild(div)
+
+    setTimeout(() => {
+      div.remove()
+    }, 3000)
+  }
+}
