28884dc94fec6fe42d4e0eb9cd0f18c204ed07bd
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-05-11 20:18:21 +0900

    Update gameScene.js

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,12 +1,46 @@
+// src/scenes/gameScene.js
+
 import { Game } from '../core/game'
-import { renderBoard } from '../ui/renderBoard'
+
+import { renderBoard }
+  from '../ui/renderBoard'
+
+import { renderHud }
+  from '../ui/hud'
+
+import { cpuMove }
+  from '../core/ai'
+
+import { checkWinner }
+  from '../core/judge'
+
+import {
+  randomMessage
+} from '../ui/message'
+
+import {
+  playMoveSound,
+  playWinSound
+} from '../ui/sounds'
 
 export function createGameScene(app) {
+
   const game = new Game()
 
   let highlights = []
 
+  let message =
+    'きみのばん！'
+
   function rerender() {
+
+    app.innerHTML = ''
+
+    renderHud(
+      app,
+      message
+    )
+
     renderBoard(
       app,
       game,
@@ -15,7 +49,66 @@ export function createGameScene(app) {
     )
   }
 
+  function finishGame(winner) {
+
+    playWinSound()
+
+    if (winner === 'player') {
+      message =
+        'かったー！'
+    }
+    else {
+      message =
+        'まけちゃった！'
+    }
+
+    rerender()
+
+    setTimeout(() => {
+
+      alert('もういっかい！')
+
+      location.reload()
+
+    }, 1200)
+  }
+
+  function enemyTurn() {
+
+    message =
+      'あいてのばん！'
+
+    rerender()
+
+    setTimeout(() => {
+
+      cpuMove(game)
+
+      playMoveSound()
+
+      const winner =
+        checkWinner(
+          game.board
+        )
+
+      if (winner) {
+        finishGame(winner)
+        return
+      }
+
+      message =
+        'きみのばん！'
+
+      rerender()
+
+    }, 800)
+  }
+
   function handleClick(x, y) {
+
+    if (game.turn !== 'player') {
+      return
+    }
 
     const isHighlighted =
       highlights.some(m =>
@@ -24,11 +117,29 @@ export function createGameScene(app) {
       )
 
     if (isHighlighted) {
+
       game.move(x, y)
+
+      playMoveSound()
 
       highlights = []
 
+      const winner =
+        checkWinner(
+          game.board
+        )
+
+      if (winner) {
+        finishGame(winner)
+        return
+      }
+
+      message =
+        randomMessage()
+
       rerender()
+
+      enemyTurn()
 
       return
     }
